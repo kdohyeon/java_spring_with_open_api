dce022bf56c2ab48f64c332f535a70c86afe45a4
kdohyeon <dhkim04@trenbe.com>
2022-10-09 14:59:45 +0900

    [LP-0] QClass 추가

--- microservice-trade/libs/adapter-persistence/src/main/java/little/pirate/autotrader/persistence/market/MarketJpaRepository.java
+++ microservice-trade/libs/adapter-persistence/src/main/java/little/pirate/autotrader/persistence/market/MarketJpaRepository.java
@@ -1,6 +1,8 @@
 package little.pirate.autotrader.persistence.market;
 
+import com.querydsl.core.BooleanBuilder;
 import little.pirate.autotrader.domain.market.Market;
+import little.pirate.autotrader.domain.market.QMarket;
 import little.pirate.autotrader.port.out.market.MarketPortV2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,8 @@ import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import static com.querydsl.jpa.JPAExpressions.selectFrom;
 
 @Repository
 public class MarketJpaRepository extends QuerydslRepositorySupport implements MarketPortV2 {
@@ -22,10 +26,19 @@ public class MarketJpaRepository extends QuerydslRepositorySupport implements Ma
         super.setEntityManager(entityManager);
     }
 
+    @Override
     public void save(Market market) {
         var entityManager = getEntityManager();
         Assert.notNull(entityManager, "Entity manager must not null.");
         entityManager.persist(market);
         entityManager.flush();
+    }
+
+    @Override
+    public Market findByMarketSymbol(String marketSymbol) {
+        QMarket qMarket = QMarket.market;
+        BooleanBuilder condition = new BooleanBuilder();
+        condition.and(qMarket.marketSymbol.eq(marketSymbol));
+        return selectFrom(qMarket).where(condition).fetchOne();
     }
 }
--- microservice-trade/libs/application/src/main/java/little/pirate/autotrader/port/out/market/MarketPortV2.java
+++ microservice-trade/libs/application/src/main/java/little/pirate/autotrader/port/out/market/MarketPortV2.java
@@ -2,8 +2,7 @@ package little.pirate.autotrader.port.out.market;
 
 import little.pirate.autotrader.domain.market.Market;
 
-import java.util.List;
-
 public interface MarketPortV2 {
     void save(Market market);
+    Market findByMarketSymbol(String marketSymbol);
 }
